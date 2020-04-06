cb9414a9de21a1bfb3b845744606f5327dd4b73d
pd4ni3l <pd.rg@outlook.com>
2020-04-05 23:22:28 -0300

    Ajustando cards

--- public/scripts/cards.js
+++ public/scripts/cards.js
@@ -1,13 +1,15 @@
 getCards()
 function getCards() {
         $.get('/vpncards', function(data){
-            if(data != null){
+            if(data != null) {
                 var newdata0 = [];
                 var newdata1 = [];
                 var newdata2 = [];
-                var conta0, conta1, conta2 = 0;
-                for ( i = 0; i < data.length; i++) {
-                    console.log('Imprime itens do FOR: ', data[i].nome_url_vpn, data[i].nome_url_vpn, data[i].tipo_acesso);
+                var conta0 = 0;
+                var conta1 = 0; 
+                var conta2 = 0;
+                for ( i = 0; i < data.length; i ++) {
+                    // console.log('Imprime itens do FOR: ', data[i].nome_url_vpn, data[i].nome_url_vpn, data[i].tipo_acesso);
                     // Contar quantos tipo_acesso tem para cada tipo_acesso
                     if (data[i].tipo_acesso == "0") {
                         conta0 ++;
@@ -16,48 +18,63 @@ function getCards() {
                     } else if (data[i].tipo_acesso == "2") {
                         conta2 ++;
                     }
-                } // FIM SOMA012
-                console.log('POS SOMA012', data[0]);
-                if (data[i].tipo_acesso == "0") {
-                    for (j = 0; j < conta0; j++) {
-                        // console.log('IF TIPO ACESSO = 0: ', data[i].nome_url_vpn, data[i].nome_url_vpn, data[i].tipo_acesso);
-                        newdata0.push( {
-                            'new_nome_url_vpn' : data[j].nome_url_vpn,
-                            'new_url_url_vpn' : data[j].nome_url_vpn,
-                            'new_tipo_acesso' : data[j].tipo_acesso
+                } // FIM CONTADOR
+                console.log('Conta 0 :', conta0);
+                console.log('Conta 1 :', conta1);
+                console.log('Conta 2 :', conta2);
+
+                for (i = 0; i < conta0; i ++) {
+                    if (data[i].tipo_acesso == "0") {
+                        // for (j = 0; j < conta0; j ++) {
+                            // console.log('IF TIPO ACESSO = 0: ', data[i].nome_url_vpn, data[i].nome_url_vpn, data[i].tipo_acesso);
+                            newdata0.push( {
+                                'new_nome_url_vpn' : data[i].nome_url_vpn,
+                                'new_url_url_vpn' : data[i].nome_url_vpn,
+                                'new_tipo_acesso' : data[i].tipo_acesso
                             });
+                        // }
                     }
-                    // Montar o CARD0
-                    showCards0(newdata0);
-                } else if (data[i].tipo_acesso == "1") {
-                    for (j = 0; j < conta1; j++) {
-                        // console.log('IF TIPO ACESSO = 1: ', data[i].nome_url_vpn, data[i].nome_url_vpn, data[i].tipo_acesso);
-                        newdata1.push( {
-                            'new_nome_url_vpn' : data[j].nome_url_vpn,
-                            'new_url_url_vpn' : data[j].nome_url_vpn,
-                            'new_tipo_acesso' : data[j].tipo_acesso
-                        }); 
+                }
+                // Montar o CARD0
+                // console.log(newdata0);
+                showCards0(newdata0);
+
+                // } else if (data[i].tipo_acesso == "1") {
+                for (i = 0; i < conta1; i ++) {
+                    console.log(data[i].tipo_acesso);
+                    if (data[i].tipo_acesso == "1") {
+                        // for (j = 0; j < conta1; j++) {
+                            // console.log('IF TIPO ACESSO = 1: ', data[i].nome_url_vpn, data[i].nome_url_vpn, data[i].tipo_acesso);
+                            newdata1.push( {
+                                'new_nome_url_vpn' : data[i].nome_url_vpn,
+                                'new_url_url_vpn' : data[i].nome_url_vpn,
+                                'new_tipo_acesso' : data[i].tipo_acesso
+                            }); 
+                        // }
                     }
-                        // Montar CARD1
-                        console.log('MONTAR CARD1');
-                } else if (data[i].tipo_acesso == "2") {
-                    for (j = 0; j < conta2; j++) {
-                        // console.log('IF TIPO ACESSO = 2: ', data[i].nome_url_vpn, data[i].nome_url_vpn, data[i].tipo_acesso);
-                        newdata2.push( {
-                            'new_nome_url_vpn' : data[i].nome_url_vpn,
-                            'new_url_url_vpn' : data[i].nome_url_vpn,
-                            'new_tipo_acesso' : data[i].tipo_acesso
-                        }); 
+                }
+                // Montar CARD1
+                console.log('CARD 1: ', newdata1);
+
+                // } else if (data[i].tipo_acesso == "2") {
+                for (i = 0; i < data.length; i ++) {
+                    if (data[i].tipo_acesso == "2") {
+                        for (j = 0; j < conta2; j ++) {
+                            // console.log('IF TIPO ACESSO = 2: ', data[i].nome_url_vpn, data[i].nome_url_vpn, data[i].tipo_acesso);
+                            newdata2.push( {
+                                'new_nome_url_vpn' : data[i].nome_url_vpn,
+                                'new_url_url_vpn' : data[i].nome_url_vpn,
+                                'new_tipo_acesso' : data[i].tipo_acesso
+                            }); 
+                        }
                     }
+                }
                         // Montar CARD2
                         console.log('MONTAR CARD2');
-                } else {
-                    console.log('FIM');
-                }
             } else {
                 console.log('Sem dados recebidos');
             }
-        })
+        });
     }
 
 function showCards0(data){
